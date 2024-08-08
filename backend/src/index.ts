fa2be3fe76f176975f5ad192fc99e729de074301
DE-Uehara <r.uehara@defide-ix.com>
2024-08-08 14:00:40 +0900

    add : test用のAPI作成

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,6 +7,10 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+app.get('/test', (c) => {
+  return c.json({ message: 'Hello' })
+})
+
 const port = 3000
 console.log(`Server is running on port ${port}`)
 
